f83577fbc38caabede973bdefaa9de00f5a59cd9
Abdullah Quraishi <quraishiabdullah1994@gmail.com>
2020-04-07 10:07:08 +0600

    jenkins error test

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
       quote:
         "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
       author: " Life"
-    },
+    }
     {
       quote:
         "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
